547be904a5453464c31e14b6d54b49083b6f0ad9
Carlos Cesar <carloslima1598@gmail.com>
2021-05-01 20:10:35 -0300

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+
 $('header a').click(function(e){
     e.preventDefault();
     var id = $(this).attr('href'),
@@ -8,3 +28,23 @@ $('header a').click(function(e){
             scrollTop: targetOffset - menuHeight
         }, 500);
 });
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass = 'animate';
+function animeScroll(){
+    const windowTop = window.pageYOffset +((window.innerHeight*3)/4);
+    target.forEach(function(element){
+        if((windowTop)>element.offsetTop){
+            element.classList.add(animationClass);
+        }else{
+            element.classList.remove(animationClass);
+        }
+    })
+}
+animeScroll();
+if(target.length){
+    window.addEventListener('scroll', debounce(function(){
+        animeScroll();
+    }, 200));
+}
