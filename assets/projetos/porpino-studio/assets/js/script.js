0322cf26b0dda4edc4da4caab6e18174500ee65b
Carlos Cesar <carloslima1598@gmail.com>
2021-06-07 21:05:05 -0300

    porpino update

--- assets/projetos/porpino-studio/assets/js/script.js
+++ assets/projetos/porpino-studio/assets/js/script.js
@@ -1,37 +1,11 @@
-/*Scroll-top*/
-const menuItens = document.querySelectorAll('header a[href^="#"]')
-menuItens.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick)
-})
-function getScrollTopByHref(element){
-    const id = element.getAttribute('href')
-    return document.querySelector(id).offsetTop
-}
-function scrollToIdOnClick(event){
-    event.preventDefault()
-    menuHeight = $('header').innerHeight()
-    const to = getScrollTopByHref(event.target) - menuHeight
-    scrollToPosition(to)
-}
-function scrollToPosition(to){
-    window.scroll({
-        top: to,
-        behavior: "smooth",
-    })
-}
+/*headerAnimation*/
+import headerAnimation from './modules/headerAnimation.js';
+headerAnimation();
 
-/*Menu-mobile*/
-const btnMobile = document.querySelector('#btn-mobile')
-btnMobile.addEventListener('click', function(){
-    const menu = document.querySelector('.menu')
-    menu.classList.toggle('active')
-})
+/*menuMobile*/
+import menuMobile from './modules/menuMobile.js';
+menuMobile();
 
-/*Header-animation*/
-function headerAnimation(){
-    window.addEventListener('scroll', function(){
-        const header = document.querySelector('header')
-        header.classList.add('hd-Animated', window.scroll > 0)
-    })
-    }
-    headerAnimation();
+/*scrollTop*/
+import scrollTop from './modules/scrollTop.js';
+scrollTop();
