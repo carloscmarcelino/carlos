9c508c1db9496e6cf56fbf86644ae2d2e02c6044
Carlos Cesar <carloslima1598@gmail.com>
2021-06-23 09:35:48 -0300

    update js

--- assets/js/modules/reveal.js
+++ assets/js/modules/reveal.js
@@ -1,30 +1,19 @@
 export default function reveal() {
-    /*Top*/
     const sr = ScrollReveal({
         origin: 'top',
         distance: '50px',
         duration: 2000,
         reset: false
     });
-
     sr.reveal('.titulo-delay',{delay: 200})
     sr.reveal('.cards-interval',{interval: 400})
-    /*Left*/
+
     const srl = ScrollReveal({
         origin: 'left',
         distance: '50px',
         duration: 2000,
         reset: false
     });
-
     srl.reveal('.titulo-delayLeft',{delay: 200})
     srl.reveal('.cards-intervalLeft',{interval: 400})
-    /*No-reset*/
-    const srNR = ScrollReveal({
-        origin: 'top',
-        distance: '50px',
-        duration: 2000,
-        reset: false
-    });
-    sr.reveal('.cards-intervalNR',{interval: 400})
 }
